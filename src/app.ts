7152e2699ab661ccd7acfa1a31950a8c921c89e8
KoryCarrera <koritocarrera@gmail.com>
2026-03-07 12:46:18 -0500

    Intregated JSON Syntax Error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { ErrorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
 
 app.use(express.json);
+
+app.use(ErrorHandler.jsonSyntxError);
 
 export default app;
